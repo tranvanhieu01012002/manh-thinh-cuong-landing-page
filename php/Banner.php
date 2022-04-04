e5d7365106e65ffa4f174c8614bda1b2e76e2ef8
HoThiVu <89339469+HoThiVu@users.noreply.github.com>
2022-04-04 15:39:40 +0700

    mình cập nhật lại file banner css và php  nha mn

--- php/Banner.php
+++ php/Banner.php
@@ -3,20 +3,20 @@
 <div class="slideshow-container">
     <!-- Kết hợp hình ảnh và nội dung cho mội phần tử trong slideshow-->
     <div class="mySlides fade">
-        <img src="https://hod.com.vn/pic/banner/1111_637072762250799097.jpg" style="height: 900px;width: 100%;">
+        <img src="https://hod.com.vn/pic/banner/1111_637072762250799097.jpg" >
     </div>
     <div class="mySlides fade ">
-        <img src="https://iweb.tatthanh.com.vn/pic/18/banner/phoi-canh-mandarin-garden-1_a5bed9276abe4f10b4f6dbd8017316c9_ca6bc566a9d6475198c87987b086e4e2.jpg "
-            style="height: 900px;width: 100%;">
+        <img src="https://iweb.tatthanh.com.vn/pic/18/banner/phoi-canh-mandarin-garden-1_a5bed9276abe4f10b4f6dbd8017316c9_ca6bc566a9d6475198c87987b086e4e2.jpg ">
 
     </div>
     <div class="mySlides fade ">
-        <img src="https://iweb.tatthanh.com.vn/pic/18/banner/01.jpg  " style="height: 900px;width: 100%;">
+        <img src="https://iweb.tatthanh.com.vn/pic/18/banner/01.jpg  " >
 
     </div>
     <div class="mySlides fade ">
         <img src=" https://iweb.tatthanh.com.vn/pic/18/banner/congvienthanhphomoiBinhDuong401.jpg"
-            style="height: 900px;width: 100%;">
+           >
+          
     </div>
 </div>
 <br>

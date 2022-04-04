3ba3f59027bbe82f00fba1f2647bd7a96e5006d4
hamongkhang <khang.ha22@student.passerellesnumeriques.org>
2022-04-05 00:31:37 +0700

    remove HTML doc just there is content

--- php/Footer.php
+++ php/Footer.php
@@ -1,56 +1,38 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-
-    <link rel="stylesheet" href="./model/css/footer.css">
-</head>
-
-<body>
-    <!-- <link rel="stylesheet" href="./model/css/footer.css"> -->
-    <div class="container-fluid" id="footer">
-        <div class="container">
-            <div id="title">
-                <h1>ĐĂNG KÍ ĐỂ NHẬN ƯU ĐÃI VÀ TƯ VẤN MIỄN PHÍ</h1>
+<link rel="stylesheet" href="./model/css/footer.css">
+<div class="container-fluid" id="footer">
+    <div class="container">
+        <div id="title">
+            <h1>ĐĂNG KÍ ĐỂ NHẬN ƯU ĐÃI VÀ TƯ VẤN MIỄN PHÍ</h1>
+        </div>
+        <div class="cont">
+            <div class="left-cont">
+                <div class="display-img">
+                    <img src="./model/img/image.jpg" alt="aaaaaaaaaaaaa">
+                </div>
+                <ul>
+                    <li>Địa chỉ: Chung cư Citizen đường 9A KDC Trung Sơn, xã Bình Hưng, huyện Bình Chánh, TPHCM</li>
+                    <br>
+                    <li>Hotline: 093 543 6677</li>
+                    <br>
+                    <li>Email: bdsphankimngoc@gmail.com</li>gi
+                </ul>
             </div>
-            <div class="cont">
-                <div class="left-cont">
-                    <div class="display-img">
-                        <img src="./model/img/image.jpg" alt="aaaaaaaaaaaaa">
-                    </div>
-                    <ul>
-                        <li>Địa chỉ: Chung cư Citizen đường 9A KDC Trung Sơn, xã Bình Hưng, huyện Bình Chánh, TPHCM</li>
-                        <br>
-                        <li>Hotline: 093 543 6677</li>
-                        <br>
-                        <li>Email: bdsphankimngoc@gmail.com</li>
-
-                    </ul>
-                </div>
-                <div class="right-cont">
-                    <br>
-                    <h2>Nhập thông tin liên hệ </h2>
-                    <br>
-                    <p class="r-cont-text">Chúng tôi đã giúp hàng nghìn khách hàng tìm được dự án ưng ý. Với quỹ hàng đa
-                        dạng nhất thị trường,
-                        chúng tôi tự tin sẽ giúp bạn thực hiện giấc mơ sơ hữu BĐS và tối ưu hiệu quả đầu tư.</p>
-                    <br>
-                    <input type="text" placeholder="Tên của bạn"> <br> <br>
-                    <input type="text" placeholder="Địa chỉ email của bạn"> <br><br>
-                    <input type="text" placeholder="Số điện thoại của bạn"> <br> <br>
-                    <textarea rows="4" cols="" placeholder="Nội dung yêu cầu dự án..."></textarea>
-                    <br> <br>
-                    <input type="submit" value="Liên hệ báo giá">
-                    <br> <br>
-                </div>
+            <div class="right-cont">
+                <br>
+                <h2>Nhập thông tin liên hệ </h2>
+                <br>
+                <p class="r-cont-text">Chúng tôi đã giúp hàng nghìn khách hàng tìm được dự án ưng ý. Với quỹ hàng đa
+                    dạng nhất thị trường,
+                    chúng tôi tự tin sẽ giúp bạn thực hiện giấc mơ sơ hữu BĐS và tối ưu hiệu quả đầu tư.</p>
+                <br>
+                <input type="text" placeholder="Tên của bạn"> <br> <br>
+                <input type="text" placeholder="Địa chỉ email của bạn"> <br><br>
+                <input type="text" placeholder="Số điện thoại của bạn"> <br> <br>
+                <textarea rows="4" cols="" placeholder="Nội dung yêu cầu dự án..."></textarea>
+                <br> <br>
+                <input type="submit" value="Liên hệ báo giá">
+                <br> <br>
             </div>
         </div>
     </div>
-
-</body>
-
-</html>
+</div>

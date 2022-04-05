189d07c92cd947fa8af98734b96109923a435e53
HoThiVu <89339469+HoThiVu@users.noreply.github.com>
2022-04-05 17:21:25 +0700

    Mình mới đẩy lên 5 file  ThankYou.css banner.css, main.css, footer.js,Footer.php nha mọi người

--- php/Footer.php
+++ php/Footer.php
@@ -1,4 +1,4 @@
-<link rel="stylesheet" href="./model/css/footer.css">
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 <div class="container-fluid" id="footer">
     <div class="container">
         <div id="title">
@@ -30,9 +30,27 @@
                 <input type="text" placeholder="Số điện thoại của bạn"> <br> <br>
                 <textarea rows="4" cols="" placeholder="Nội dung yêu cầu dự án..."></textarea>
                 <br> <br>
-                <input type="submit" value="Liên hệ báo giá">
+                <button type="submit" value="">Liên hệ báo giá</button>
                 <br> <br>
             </div>
+            <div class="modal-overlay">
+                    <div class="modal">
+                        <a class="close-modal">
+                            <svg viewBox="0 0 20 20">
+                                <path fill="#000000" d="M15.898,4.045c-0.271-0.272-0.713-0.272-0.986,0l-4.71,4.711L5.493,4.045c-0.272-0.272-0.714-0.272-0.986,0s-0.272,0.714,0,0.986l4.709,4.711l-4.71,4.711c-0.272,0.271-0.272,0.713,0,0.986c0.136,0.136,0.314,0.203,0.492,0.203c0.179,0,0.357-0.067,0.493-0.203l4.711-4.711l4.71,4.711c0.137,0.136,0.314,0.203,0.494,0.203c0.178,0,0.355-0.067,0.492-0.203c0.273-0.273,0.273-0.715,0-0.986l-4.711-4.711l4.711-4.711C16.172,4.759,16.172,4.317,15.898,4.045z"></path>
+                            </svg>
+                        </a>
+                        <!-- close modal -->
+                        <div class="modal-content">
+                            <h2 class="text-h1">Cảm ơn bạn đã đăng ký!</h2>
+                               
+                            <h3>Chúng tôi sẽ Liên hệ bạn sớm nhất.</h3> 
+                        </div>
+                        <!-- content -->
+                    </div>
+                    <!-- modal -->
+                </div>
         </div>
     </div>
 </div>
+<script src="./model/js/footer.js"></script>

ed935d8e2317898925d58338efe23b012d3ec0af
tamdinh793@gmail.com <tamdinh793@gmail.com>
2022-04-18 00:21:51 +0700

    Tam sua tiep ne

--- Admin/timeline/indexAdmin.php
+++ Admin/timeline/indexAdmin.php
@@ -58,7 +58,7 @@
                 </a>
             </li>
             <li>
-                <a href="../sildes/indexAdmin.php">
+                <a href="../project/indexAdmin.php">
                     <i class='bx bx-pie-chart-alt-2'></i>
                     <span class="links_name">House</span>
                 </a>
@@ -82,7 +82,7 @@
                     <div class="dropdown-menu dropdown-menu-right" aria-labelledby="userDropdown">
                         <a class="dropdown-item" href="#">Settings</a><a class="dropdown-item" href="#">Activity Log</a>
                         <div class="dropdown-divider"></div>
-                        <a class="dropdown-item" onclick="logout()">Logout</a>
+                        <a class="dropdown-item" href="../logout.php">Logout</a>
                     </div>
                 </li>
             </ul>

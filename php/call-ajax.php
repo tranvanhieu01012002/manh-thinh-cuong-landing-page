abc46ca6ad6c37e638a1df7b9abab8c3baf7b51e
tranvanhieu2002 <hieu.tran23@student.passerellesnumeriques.org>
2022-04-18 23:42:09 +0700

    upload Ajax to call text when click

--- php/call-ajax.php
+++ php/call-ajax.php
@@ -1,9 +1,17 @@
 <?php
+include("../Admin/dph.php");
+
 if(isset($_POST["data"])){
-    // $data = new dph();
-    // $data->get_row("select * from ")
-    echo $_POST["data"];
+    $dataa = new dph();
+    $row = $dataa->get_row("select * from text_time_line where id = '31'");
+    $i = 0;
+    foreach($row as $value){
+        if($i==$_POST["data"] ){
+            echo $value;
+            break;
+        }
+        $i++;
+    }
+    echo $i;
 }
-
-
 ?>
